b1311f384777bafb62e92eb0d88f8b2d672756a2
ilia <aliwe2@yandex.ru>
2023-02-27 02:30:40 +0400

    add user model, serializer, urls

---
+++ userapp/models.py
@@ -0,0 +1,9 @@
+from django import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    username = models.CharField(max_length=64, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
